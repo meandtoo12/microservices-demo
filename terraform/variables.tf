a4f558db32968985664bdec89b55371702f2003a
me <metoo12@gmail.com>
2024-08-15 10:42:37 +0000

    edited terraform apply to use values from github

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
