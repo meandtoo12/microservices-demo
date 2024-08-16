7b0987d673a4f50cfcc497aef6131d7faaf4c6be
me <metoo12@gmail.com>
2024-08-16 10:46:04 +0000

    ADDED LOGIN2

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,17 +10,15 @@ terraform {
       storage_account_name = "mytfstateforazure"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
+      use_msi              = true  
   }
   
 }
 
 provider "azurerm" {
   features {}
-
   subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  use_msi         = true
 }
 
 
